375770724f8ec674fc0223466f521b0b2c7f4ed5
Mike Monaco <mamonaco1973@gmail.com>
2025-01-30 18:57:22 -0500

    comment acr.tf

--- 01-acr/main.tf
+++ 01-acr/main.tf
@@ -10,7 +10,7 @@ data "azurerm_subscription" "primary" {}
 # Data source to fetch the details of the current Azure client
 data "azurerm_client_config" "current" {}
 
-# Define a resource group for all resources in this setup
+# Define a resource group for all resources in this project
 resource "azurerm_resource_group" "flask_container_rg" {
   name     = var.resource_group_name  # Name of the resource group
   location = "Central US"             # Region where resources will be deployed

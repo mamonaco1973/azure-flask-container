624716c5115ddb2f17268c1964f6dd6a4a7b7409
mamonaco1973 <mamonaco1973@gmail.com>
2025-02-01 23:32:54 +0000

    remove backends

--- 03-containerapp/03-containerapp-backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "terraform-backend-jcuwxn"
-    storage_account_name  = "tfstatejcuwxn"
-    container_name        = "tfstate"
-    key                  = "03-containerapp/terraform.tfstate.json"
-  }
-}
